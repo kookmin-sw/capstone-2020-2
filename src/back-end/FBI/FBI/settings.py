9e246cd0632ce8c4c0b899812374468924e6f532
Myounghee Seo <myounghee.seo2@gmail.com>
2020-04-17 19:37:27 +0900

    fix: Setting corsheaders to link backend-frontend

--- src/back-end/FBI/FBI/settings.py
+++ src/back-end/FBI/FBI/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
 AUTH_USER_MODEL = 'api.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/',
+)
 
 ROOT_URLCONF = 'FBI.urls'
 
