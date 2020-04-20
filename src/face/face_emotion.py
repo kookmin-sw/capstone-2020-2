811009ff8241e14af650bfba4bc3035bf376457e
leeyell <lylim5050@kookmin.ac.kr>
2020-04-20 19:05:20 +0900

    Modifying model structure

--- src/face/face_emotion.py
+++ src/face/face_emotion.py
@@ -1,7 +1,6 @@
 # import
 import os
 import numpy as np
-import pandas as pd
 import cv2
 import torch
 import torch.nn as nn
@@ -10,74 +9,26 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader, Dataset
 
-# GPU 설정
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# GPU 설정 확인
-print(torch.cuda.is_available())
 
 
 
-# 하이퍼 파라미터 설정 (일단 아무값이나 주고...)
-num_epochs = 10
-num_classes = 7
-batch_size = 128
-learning_rate = 0.001
-
-'''
-class MyDataset(Dataset):
-    def __init__(self, df_data, data_dir="./", transform=None):
-        super().__init__()
-        self.df = df_data.values
-        self.data_dir = data_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, index):
-        img_name, label = self.df[index]
-        img_path = os.path.join(self.data_dir, img_name + ".jpg")
-        image = cv2.imread(img_path)
-        
-        if self.transform is not None:
-            image = self.transform(image)
-
-        return image, label
-'''
-
-trans1 = transforms.Compose([transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                            ])
-trans2 = transforms.Compose([transforms.resize(64, 64),
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                            ])
-train_dataset_path = "dataset/Face_expression_recognition_dataset/images/train"
-train_dataset = torchvision.datasets.ImageFolder(root=train_dataset_path, transform=trans1)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-
-    
-
-############ 기존 코드에서 stride 안 줬을 때 디폴트 값 몇인지 찾아보기..
-############ 케라스 Conv2D 랑 SeparableConv2D
 class FaceEmotion(nn.Module):
     def __init__(self, num_classes=7):
         super(FaceEmotion, self).__init__()
 
         # base block
         self.base_block = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3),
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3),
+            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3,),
             nn.BatchNorm2d(8),
             nn.ReLU(inplace=True))
 
         # Residual black 1 - shortcut
         self.shortcut_1 = nn.Sequential(
-            nn.BatchNorm2d(16),
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=1, stride=2, bias=False))
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(16))
 
         # Residual block 1
         self.residual_block_1 = nn.Sequential(
@@ -92,8 +43,8 @@ class FaceEmotion(nn.Module):
 
         # Residual black 2 - shortcut
         self.shortcut_2 = nn.Sequential(
-            nn.BatchNorm2d(32),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=1, stride=2, bias=False))
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(32))
 
         # Residual block 2
         self.residual_block_2 = nn.Sequential(
@@ -108,8 +59,8 @@ class FaceEmotion(nn.Module):
 
         # Residual black 3 - shortcut
         self.shortcut_3 = nn.Sequential(
-            nn.BatchNorm2d(64),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2, bias=False))
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(64))
 
         # Residual block 3
         self.residual_block_3 = nn.Sequential(
@@ -124,8 +75,8 @@ class FaceEmotion(nn.Module):
 
         # Residual black 4 - shortcut
         self.shortcut_4 = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2, bias=False))
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(128))
 
         # Residual block 4
         self.residual_block_4 = nn.Sequential(
@@ -145,11 +96,18 @@ class FaceEmotion(nn.Module):
         )
 
     def forward(self, x):
+        print(x.shape)
         x = self.base_block(x)
-        x = self.residual_block_1(x) + shortcut_1(x)
-        x = self.residual_block_2(x) + shortcut_2(x)
-        x = self.residual_block_3(x) + shortcut_3(x)
-        x = self.residual_block_4(x) + shortcut_4(x)
+        print(x.shape)
+        res_x_1 = self.residual_block_1(x)
+        print("res_x_1 shape", res_x_1.shape)
+        short_x_1 = self.shortcut_1(x)
+        print("short_x_1 shape", short_x_1.shape)
+        x = res_x_1 + short_x_1
+        print(x.shape)
+        x = self.residual_block_2(x) + self.shortcut_2(x)
+        x = self.residual_block_3(x) + self.shortcut_3(x)
+        x = self.residual_block_4(x) + self.shortcut_4(x)
 
         x = self.last_block(x)
 
@@ -159,3 +117,59 @@ class FaceEmotion(nn.Module):
         output = F.softmax(x, dim=0)
 
         return output
+
+
+
+# GPU 설정
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# GPU 설정 확인
+print(torch.cuda.is_available())
+
+# 하이퍼 파라미터 설정
+num_epochs = 10
+num_classes = 7
+batch_size = 128
+learning_rate = 0.001
+
+
+trans1 = transforms.Compose([transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                            ])
+'''
+trans2 = transforms.Compose([transforms.resize(64, 64),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                            ])
+'''
+train_dataset_path = "dataset/Face_expression_recognition_dataset/images/train"
+train_dataset = torchvision.datasets.ImageFolder(root=train_dataset_path, transform=trans1)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    
+
+# model 객체 생성
+model = FaceEmotion()
+print("Network 생성")
+print(model)
+model.cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+total_step = len(train_loader)
+for epoch in range(0, num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backword()
+        optimizer.step()
+        
+        if (i%10) == 0:
+            print(f"Epoch [{i}/{num_epochs}], Step [{i+1}/{total_step}, Loss : {loss.item}")
+
+print("학습 끝. 모델 저장.")
+torch.save(model, "FaceEmotionModel.pt")
