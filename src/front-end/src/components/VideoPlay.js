e1f170c52dc5c444c932bc3af5ec60aff3ce0db8
sojung1418 <sjk1418@naver.com>
2020-04-22 11:36:26 +0900

    feat:add chart in videoplay

--- src/front-end/src/components/VideoPlay.js
+++ src/front-end/src/components/VideoPlay.js
@@ -5,7 +5,33 @@ import {Link, BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Webcam from "react-webcam";
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-
+import {
+	BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
+  
+  const data = [
+	{
+	  name: 'Happiness', pv: 2400, amt: 2400,
+	},
+	{
+	  name: 'anger', pv: 1398, amt: 2210,
+	},
+	{
+	  name: 'fear',  pv: 9800, amt: 2290,
+	},
+	{
+	  name: 'disgust', pv: 3908, amt: 2000,
+	},
+	{
+	  name: 'sadness', pv: 4800, amt: 2181,
+	},
+	{
+	  name: 'surprise',  pv: 3800, amt: 2500,
+	},
+	{
+	  name: 'neutral', pv: 4300, amt: 2100,
+	},
+  ];
 
 
 class VideoPlay extends Component {
@@ -35,7 +61,18 @@ class VideoPlay extends Component {
 				facingmode="user"
 				ref={this.setRef}
 						/>
-						</>
+
+		<BarChart width={300} height={300} data ={data}  
+ 		  barSize={20}
+    			  > <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+				
+				  <Tooltip />
+				  <Legend />
+				  <CartesianGrid strokeDasharray="3 3" />
+				  <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+				</BarChart>
+
+					</>
 		);
 	} 
 }
