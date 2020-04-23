7d07def7e9a5925f492e776d37113f82e24c5e15
leeyell <lylim5050@kookmin.ac.kr>
2020-04-23 10:33:25 +0900

    refactor : Add code to learn and evaluate model.
    Optimizer or Loss Function, and the structure of the model is likely to continue to change.

--- src/face/face_emotion.py
+++ src/face/face_emotion.py
@@ -8,17 +8,66 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader, Dataset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+
+
+dataset_path = "dataset/FER2013/fer2013/fer2013.csv"
+image_size = (48,48)
+
+# 하이퍼 파라미터 설정
+batch_size = 32
+# num_epochs = 110
+num_epochs = 10
+input_shape = (1, 48, 48)
+validation_split = 0.2
+verbose = 1
+num_classes = 7
+patience = 50
+base_path = 'models/'
+learning_rate = 0.001
+l2_regularization = 0.01
+
+
+
+class MyDataset(Dataset):
+    def __init__(self, df_data, transform=None):
+        super().__init__()
+        pixels = df_data['pixels'].tolist()
+        width, height = 48, 48
+        faces = []
+        for pixel_sequence in pixels:
+            face = [int(pixel) for pixel in pixel_sequence.split(' ')]
+            face = np.asarray(face).reshape(1, width, height)
+            # face = cv2.resize(face.astype('uint8'), image_size)
+            faces.append(face.astype('float32'))
+        
+        self.faces = np.asarray(faces)
+        # self.faces = np.expand_dims(faces, -1)
+        # self.emotions = pd.get_dummies(df_data['emotion']).values
+        self.emotions = df_data['emotion'].values
+        self.transform = transform
+        print(self.faces[0].shape)
+
+    def __len__(self):
+        return self.emotions.shape[0]
+    
+    def __getitem__(self, index):
+        return self.faces[index], self.emotions[index]
 
 
 
 
 class FaceEmotion(nn.Module):
-    def __init__(self, input_channel=3, num_classes=7):
+    def __init__(self, num_classes=num_classes):
         super(FaceEmotion, self).__init__()
 
         # base block
         self.base_block = nn.Sequential(
-            nn.Conv2d(in_channels=input_channel, out_channels=8, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, stride=1, padding=1, bias=False),
@@ -27,84 +76,81 @@ class FaceEmotion(nn.Module):
 
         # Residual black 1 - shortcut
         self.shortcut_1 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(16))
 
         # Residual block 1
         self.residual_block_1 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1,bias=False),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(16),
 
-            nn.MaxPool2d(kernel_size=3, stride=2))
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        )
 
         # Residual black 2 - shortcut
         self.shortcut_2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32))
 
         # Residual block 2
         self.residual_block_2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1,bias=False),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
 
-            nn.MaxPool2d(kernel_size=3, stride=2))
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        )
 
         # Residual black 3 - shortcut
         self.shortcut_3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64))
 
         # Residual block 3
         self.residual_block_3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1,bias=False),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
 
-            nn.MaxPool2d(kernel_size=3, stride=2))
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        )
 
         # Residual black 4 - shortcut
         self.shortcut_4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(128))
 
         # Residual block 4
         self.residual_block_4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1,bias=False),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(128),
 
-            nn.MaxPool2d(kernel_size=3, stride=2))
-        
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        )
+
         # 아웃 채널은 클래스의 수 7가지...
         self.last_block = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=7, kernel_size=3),
+            nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3),
             nn.AdaptiveAvgPool2d((1, 1))        # 인자로 주는 값은 결과물의 H x W : 전역평균
         )
 
     def forward(self, x):
-        print("origin shape", x.shape)
         x = self.base_block(x)
-        print("base_block shape", x.shape)
-        res_x_1 = self.residual_block_1(x)
-        print("res_x_1 shape", res_x_1.shape)
-        short_x_1 = self.shortcut_1(x)
-        print("short_x_1 shape", short_x_1.shape)
-        x = res_x_1 + short_x_1
-        print(x.shape)
+        x = self.residual_block_1(x) + self.shortcut_1(x)
         x = self.residual_block_2(x) + self.shortcut_2(x)
         x = self.residual_block_3(x) + self.shortcut_3(x)
         x = self.residual_block_4(x) + self.shortcut_4(x)
@@ -123,53 +169,74 @@ class FaceEmotion(nn.Module):
 # GPU 설정
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # GPU 설정 확인
-print(torch.cuda.is_available())
+print("GPU : ", torch.cuda.is_available())
 
-# 하이퍼 파라미터 설정
-num_epochs = 10
-num_classes = 7
-batch_size = 128
-learning_rate = 0.001
-
-
-trans1 = transforms.Compose([transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+# Dataset 불러오기
+print("Dataset Loading...")
+trans_train = transforms.Compose([transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            transforms.RandomRotation(10),
+                            transforms.RandomHorizontalFlip(),
                             ])
-'''
-trans2 = transforms.Compose([transforms.resize(64, 64),
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+trans_val = transforms.Compose([transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                             ])
-'''
-train_dataset_path = "dataset/Face_expression_recognition_dataset/images/train"
-train_dataset = torchvision.datasets.ImageFolder(root=train_dataset_path, transform=trans1)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
-    
+data = pd.read_csv(dataset_path)
+print(f"Dataset size : {len(data)}")
+train, test = train_test_split(data, test_size=0.2, shuffle=True)
+train, val = train_test_split(train, test_size=validation_split, shuffle=False)
+print(f"train : {len(train)} + val {len(val)} / test : {len(test)}")
+
+dataset_train = MyDataset(df_data=train, transform=trans_train)
+dataset_val = MyDataset(df_data=val, transform=trans_val)
+
+loader_train = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=False, num_workers=0)
+loader_val = DataLoader(dataset=dataset_val, batch_size=batch_size//2, shuffle=False,num_workers=0)
+
+
 
 # model 객체 생성
 model = FaceEmotion()
 print("Network 생성")
-print(model)
+# print(model)
 model.cuda()
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=l2_regularization)
 
-total_step = len(train_loader)
+total_step = len(loader_train)
 for epoch in range(0, num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
+    for i, (images, labels) in enumerate(loader_train):
         images = images.to(device)
         labels = labels.to(device)
-
         outputs = model(images)
+
         loss = criterion(outputs, labels)
 
         optimizer.zero_grad()
-        loss.backword()
+        loss.backward()
         optimizer.step()
         
-        if (i%10) == 0:
-            print(f"Epoch [{i}/{num_epochs}], Step [{i+1}/{total_step}, Loss : {loss.item}")
+        if (i%50) == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss : {loss.item()}")
+            print(classification_report(labels, outputs, target_names=['class 1', 'class 2', 'class 3',
+                                                'class 4', 'class 5', 'class 6', 'class 7']))
+
+
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in loader_val:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print(f"Accuracy of the model for validation : {100 * correct / total}")
+
 
 print("학습 끝. 모델 저장.")
 torch.save(model, "FaceEmotionModel.pt")
