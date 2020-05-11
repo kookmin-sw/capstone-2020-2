fc0691dd39f5c8ca8482180d396adef201dd0e4c
Myounghee Seo <myounghee.seo2@gmail.com>
2020-05-11 20:05:16 +0900

    refactor: Split loginSuccessAlert to component.

--- src/front-end/src/pages/Option.js
+++ src/front-end/src/pages/Option.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
-// import {Spinner, Button, Label} from "reactstrap";
+import LoginAlert from '../components/loginSuccessAlert';
+import { Button, Grid, withStyles, Typography } from '@material-ui/core/';
 import {
-  Button,
-  Grid,
-  withStyles,
-  Typography,
-  IconButton,
-} from '@material-ui/core/';
-import { Alert, AlertTitle } from '@material-ui/lab/';
-import Collapse from '@material-ui/core/Collapse';
-import CloseIcon from '@material-ui/icons/Close';
-
-// import Button from "@material-ui/core/Button";
-// import Grid from '@material-ui/core/Grid';
-import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+  Link,
+  withRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 const style = {
   btnStyle: {
@@ -39,29 +33,7 @@ class Option extends Component {
 
     return (
       <div class="container-fluid">
-        <Collapse in={this.state.close}>
-          <Alert
-            severity="info"
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  this.setState({
-                    close: false,
-                  });
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-          >
-            <AlertTitle> Login! </AlertTitle>{' '}
-            {this.props.location.state.userName}님 안녕하세요!{' '}
-            <strong> </strong>{' '}
-          </Alert>{' '}
-        </Collapse>{' '}
+        <LoginAlert userName={this.props.location.state.userName}></LoginAlert>
         <Grid
           container
           spacing={10}
@@ -101,4 +73,4 @@ class Option extends Component {
 }
 
 Option.propTypes = {};
-export default withStyles(style)(Option);
+export default withRouter(withStyles(style)(Option));
